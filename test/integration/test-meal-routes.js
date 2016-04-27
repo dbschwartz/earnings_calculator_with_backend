d3449e6041916223f9913b6709dcedcc39b369e9
David Schwartz <dbschwartz2@gmail.com>
2016-04-27 15:59:54 -0600

    Wrote Tests For Routes

--- test/integration/test-meal-routes.js
+++ test/integration/test-meal-routes.js
@@ -9,68 +9,67 @@ var should = chai.should();
 
 chai.use(chaiHttp);
 
-describe('API routes', function(){
+describe('API routes', function() {
 
-  beforeEach(function(done) {
-    knex.migrate.rollback.then(function() {
-        knex.migrate.latest()
-          .then(function() {
-              return knex.seed.run().then(function() {
-                  done()
-               });
-          })
+    beforeEach(function(done) {
+        knex.migrate.rollback.then(function() {
+            knex.migrate.latest()
+                .then(function() {
+                    return knex.seed.run().then(function() {
+                        done()
+                    });
+                })
+        });
     });
-  });
 
-  afterEach(function(done) {
-    knex.migrate.rollback().then(function() {
-        done();
-    });
-  });
-
-  describe('Get all meals', function() {
-
-      it('should get all meals', function(done){
-          chai.request(server)
-          .get('/meals')
-          .end(function(err, res)  {
-            res.status.should.equal(200);
-            res.type.should.equal('application/json');
-            res.body.should.be.a('object');
-            res.body.should.have.property('status');
-            res.body.should.have.property('data');
-            res.body.status.should.equal('success');
-            res.body.data.should.be.a('array');
-            res.body.data.length.should.equal(2);
-            res.body.data[0].base_price.should.equal(25.50);
-            res.body.data[0].tax_rate.should.equal(8.25);
-            res.body.data[0].tip_rate.should.equal(15);
+    afterEach(function(done) {
+        knex.migrate.rollback().then(function() {
             done();
-          });
-      })
-  });
-    describe('/POST a meal', function() {
-      it('should create a new meal', function(done) {
-        chai.request(server)
-        .post('/addmeal')
-        .send({
-          base_price: 40.50,
-          tax_rate: 8.25,
-          tip_rate: 20
+        });
+    });
+
+    describe('Get all meals', function() {
+        it('should get all meals', function(done) {
+            chai.request(server)
+                .get('/meals')
+                .end(function(err, res) {
+                    res.status.should.equal(200);
+                    res.type.should.equal('application/json');
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('status');
+                    res.body.should.have.property('data');
+                    res.body.status.should.equal('success');
+                    res.body.data.should.be.a('array');
+                    res.body.data.length.should.equal(2);
+                    res.body.data[0].base_price.should.equal(25.50);
+                    res.body.data[0].tax_rate.should.equal(8.25);
+                    res.body.data[0].tip_rate.should.equal(15);
+                    done();
+                });
         })
-        .end(function(err, res) {
-          res.status.should.equal(200);
-          res.type.should.equal('application/json');
-          res.body.should.be.a('object');
-          res.body.should.have.property('status');
-          res.body.should.have.property('data');
-          res.body.status.should.equal('success');
-          res.body.data.should.be.a('object');
-          res.body.data.base_price.should.equal(40.50);
-          res.body.data.tax_rate.should.equal(8.25);
-          res.body.data.tip_rate.should.equal(20);
-          done();
-         });
-      });
+    });
+    describe('/POST a meal', function() {
+        it('should create a new meal', function(done) {
+            chai.request(server)
+                .post('/addmeal')
+                .send({
+                    base_price: 40.50,
+                    tax_rate: 8.25,
+                    tip_rate: 20
+                })
+                .end(function(err, res) {
+                    res.status.should.equal(200);
+                    res.type.should.equal('application/json');
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('status');
+                    res.body.should.have.property('data');
+                    res.body.status.should.equal('success');
+                    res.body.data.should.be.a('object');
+                    res.body.data.base_price.should.equal(40.50);
+                    res.body.data.tax_rate.should.equal(8.25);
+                    res.body.data.tip_rate.should.equal(20);
+                    done();
+                });
+        });
     });
 });

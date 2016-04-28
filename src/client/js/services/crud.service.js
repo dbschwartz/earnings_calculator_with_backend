c40cff93359b70cca16d2590f18be80299e462f4
David Schwartz <dbschwartz2@gmail.com>
2016-04-28 10:32:11 -0600

    Added Crud Service

--- src/client/js/services/crud.service.js
+++ src/client/js/services/crud.service.js
@@ -14,8 +14,26 @@ crudService.$inject = ['$http'];
 
 function crudService($http) {
 
+  return {
 
-
-
-
+    getMeals: function(){
+      return $http.get('/meals')
+        .then(function(res){
+          return res;
+        })
+        .catch(function(err){
+          return err;
+        });
+    },
+    addMeal: function(meal){
+      return $http.post('/addmeal', meal)
+        .then(function(res){
+           return res;
+        })
+        .catch(function(err){
+          return err;
+        });
+    }
+    
+  };
 }
